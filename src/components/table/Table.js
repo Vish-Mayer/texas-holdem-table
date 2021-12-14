a1d3849a2be6c9d9e2d499511451471fc2c76b8c
Vish Mayer <vish.mayer@gmail.com>
2021-12-14 14:25:26 +0000

    styling - created outer table to position players

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,31 +2,37 @@ import "./Table.css";
 import { Card } from "../card/Card";
 import { Button } from "../button/Button";
 import { useDealHand } from "../../hooks/useDealHand";
+import { useTransition } from "react-spring";
 
 export const Table = () => {
   const { dealNewHand, result, isLoading } = useDealHand();
 
   return (
-    <div className="tableStyles">
-      <div className="board">
-        {isLoading ? (
-          <h4 className="loading">Dealing Cards...</h4>
-        ) : (
-          <div>
-            {result &&
-              result.board.map((card, idx) => {
-                return (
-                  <div className="cardStyle-small" key={idx}>
-                    <Card face={card[0]} suit={card[1]} />
-                  </div>
-                );
-              })}
+    <body>
+      <div className="outerTable">
+        <div class="number number1">1</div>
+        <div className="innerTable">
+          <div className="board">
+            {isLoading ? (
+              <h4 className="loading">Dealing Cards...</h4>
+            ) : (
+              <div>
+                {result &&
+                  result.board.map((card, idx) => {
+                    return (
+                      <div className="cardStyle-small" key={idx}>
+                        <Card face={card[0]} suit={card[1]} />
+                      </div>
+                    );
+                  })}
+              </div>
+            )}
+            <div className="dealer-btn">
+              <Button onClick={dealNewHand} name={"Deal New Hand"} />
+            </div>
           </div>
-        )}
-        <div className="dealer-button">
-          <Button onClick={dealNewHand} name={"Deal New Hand"} />
         </div>
       </div>
-    </div>
+    </body>
   );
 };
